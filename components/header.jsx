c0cad4137063d02427e1adf9eadc7bbdc41f9687
Ftnn319 <feitan0125@gmail.com>
2025-04-12 14:34:30 +0800

    up

---
+++ components/header.jsx
@@ -0,0 +1,43 @@
+"use client"
+
+import Link from "next/link"
+import MobileMenu from "./mobilemenu"
+import { Button } from "./ui/button"
+
+export default function Header({onNavigate}) {
+  return (
+    <header className="border-b-8 border-white sticky top-0 bg-gray-100 z-10">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          <Link href="/" className="text-lg font-bold">
+            Baljinnyam
+          </Link>
+
+          <nav className="hidden md:flex space-x-8 cursor-pointer">
+            <div onClick={()=>onNavigate("home")} className="text-gray-900 hover:text-blue-500 transition-colors">
+              Home
+            </div>
+            <div onClick={()=>onNavigate("about")} className="text-gray-900 hover:text-blue-500 transition-colors">
+              About
+            </div>
+            <div onClick={()=>onNavigate("portfolio")} className="text-gray-900 hover:text-blue-500 transition-colors">
+              Portfolio
+            </div>
+            <div onClick={()=>onNavigate("price")} className="text-gray-900 hover:text-blue-500 transition-colors">
+              Price
+            </div>
+            <div onClick={()=>onNavigate("blog")} className="text-gray-900 hover:text-blue-500 transition-colors">
+              Blog
+            </div>
+          </nav>
+          <div className="flex items-center space-x-4">
+            <Button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition-colors hidden md:block">
+              Let's Talk
+            </Button>
+            <MobileMenu />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ components/hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero(){
+    return (
+        <div className="flex gap-6 flex-col items-center p-4 lg:p-16 bg-gray-100 rounded-lg">
+        <p className="text-black text-2xl lg:text-5xl font-bold text-center">
+          D.Baljinnyam <br/> Software Engineering
+        </p>  
+        <p className="py-3 px-6 w-full lg:w-[500px] text-gray-300 font-normal  text-base bg-white lg:p-10 rounded-2xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque maximus aliquam. Praesent feugiat mi sed scelerisque bibendum. Phasellus feugiat dignissim metus id ultrices. 
+        </p>
+      </div>
+    )
+}
