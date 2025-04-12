48c1d658c70dfebd6836e698f4450a3a71502b90
Ftnn319 <feitan0125@gmail.com>
2025-04-12 14:46:10 +0800

    changes

--- components/hero.jsx
+++ components/hero.jsx
@@ -5,7 +5,7 @@ export default function Hero(){
           D.Baljinnyam <br/> Software Engineering
         </p>  
         <p className="py-3 px-6 w-full lg:w-[500px] text-gray-300 font-normal  text-base bg-white lg:p-10 rounded-2xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque maximus aliquam. Praesent feugiat mi sed scelerisque bibendum. Phasellus feugiat dignissim metus id ultrices. 
+        My name is Baljinnyam, and I am in my first year studying Software Engineering at the National University of Mongolia.
         </p>
       </div>
     )
